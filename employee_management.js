a633ba54f87a3f3e8234cb3dd26a6a1a08c92c71
Leslie Rodriguez <leslie286@usf.edu>
2024-10-06 21:54:59 -0400

    Create Employee class with properties and methods

---
+++ employee_management.js
@@ -0,0 +1,28 @@
+// Task 1: Create an Employee Class
+class Employee:{
+def init(self, name, salary, position, department):
+{ 
+    this.name = "Alisa Jones";
+    this.department = "Marketing";
+    this.position = "Manager";
+    this.salary = "50000";
+    String details = employee.getDetails("Alisa Jones", "Manager", $"50000");
+}
+{   
+    this.name = "John Adams";
+    this.department = "IT";
+    this.position = "SoftwareDeveloper";
+    this.salary= "80000";
+    String details = employee.getDetails("John Adams", "SoftwareDeveloper", $"80000");
+}
+{ 
+    this.name = "James Johnson";
+    this.department = "Engineering";
+    this.position = "Designer";
+    this.salary = 90000
+    String details = employee.getDetails("James Johnson", "Designer", $"90000");
+}
+
+
+
+}
