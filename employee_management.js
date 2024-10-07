0710ffacf8a1c464aa2a1a696cff8d718f8da451
Leslie Rodriguez <leslie286@usf.edu>
2024-10-06 23:03:55 -0400

    Handle bonuses in the department salary calculation

--- employee_management.js
+++ employee_management.js
@@ -42,7 +42,23 @@ console.log(`Total salary for marketing: $${marketing.getDepartmentSalary()}`);
 console.log(`Total salary for SoftwareDeveloper:$${SoftwareDeveloper.getDepartmentSalary()}`)
 console.log(`Total salry for Designer:$${Designer.getDepartmentSalary()}`)
 
+//Task 3:Create a Manager Class that Inherits from Employee
+//Task 4: Handle Bonuses for Managers
 
 
+//Employee class
+employee= ("John", "80000");
+employee= ("James", 90000);
+ 
+//Manager class
+manager= ("Alisa", "50000")
+this.bonus= 10000;
+for (Employee employee: employees) {
+    totalSalary += employee.getSalary();
+    if (employee instanceof Manager) {
+        totalSalary += ((Manager) employee).getBonus();
 
+    }
+}
+return totalSalary;
 }
